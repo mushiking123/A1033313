bf4becf14309dcf30d991e17ee4cf7e46b14644e
kevin <firefox1937@gmail.com>
2016-03-17 14:53:50 +0800

    小考

--- practice_4.py
+++ practice_4.py
@@ -5,14 +5,16 @@ def isPrime(n):
 	for x in range(1,n+1):
 		if(n%x==0):
 			num=num+1
-			if(num==2):
-				return 1
-			else:
-				return 0
-for x in range(1,1001,2):
-	sum=0
-	a=isPrime(x)
-	if a==1:
+	if(num==2):
+		return 1
+	else:
+		return 0 
+
+a=isPrime(5)
+print a		
+sum=0	
+for x in range(1,1001):
+	if isPrime(x)==1:
 		sum=sum+x
 print "%d" %sum
 		
