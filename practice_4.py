2161c6428f71443f8e6dd5b1c549d23b0562d109
kevin <firefox1937@gmail.com>
2016-03-17 14:28:11 +0800

    小考

---
+++ practice_4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def isPrime(n):
+	num=0
+	for x in range(1,n+1):
+		if(n%x==0):
+			num=num+1
+			if(num==2):
+				return 1
+			else:
+				return 0
+for x in range(1,1001,2):
+	sum=0
+	a=isPrime(x)
+	if a==1:
+		sum=sum+x
+print "%d" %sum
+		
+		
