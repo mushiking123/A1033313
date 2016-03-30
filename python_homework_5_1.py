4d663d81adc913e3d4dde3bd7c93a5d37e4597c2
kevin <firefox1937@gmail.com>
2016-03-30 23:54:15 +0800

    第五次作業

---
+++ python_homework_5_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+str="honey, you are always on my mind. your love makes me blind .when I first met you I knew it was a sign."
+name={'honey':'mary','lynn':'baby','jannie':'little sweetie'}
+str1=str.replace('honey',name['honey'])
+print str1
